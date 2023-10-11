6aa46e9a88c449c5921b87421a41e991c8a26ca2
oscarLS-11 <oluna1103@gmail.com>
2023-10-10 23:57:51 -0600

    cambios diseño

--- app/src/main/java/com/example/faunaspyv1/UserActivity.java
+++ app/src/main/java/com/example/faunaspyv1/UserActivity.java
@@ -6,14 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class UserActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_user);
     }
-
 
     public void openHome(View view) {
         Intent intent = new Intent(view.getContext(), MainActivity.class);
